23590f7feeede832f4aa27b6dfde3c376eb1f775
junihoj <ezeonyekachukwu98@gmail.com>
2023-07-16 16:50:31 +0100

    places screen

---
+++ components/places/PlaceForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+const PlaceForm = () => {
+  return (
+    <View>
+      <Text>PlaceForm</Text>
+    </View>
+  );
+};
+
+export default PlaceForm;
---
+++ screens/AllPlaces.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text } from "react-native";
+import PlacesList from "../components/places/PlacesList";
+
+const AllPlaces = () => {
+  return <PlacesList />;
+};
+
+export default AllPlaces;
